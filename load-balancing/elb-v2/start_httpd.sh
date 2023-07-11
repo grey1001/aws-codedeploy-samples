c88f587eb218f532d748fa974d87f1331cb6a5f4
grey1001 <130705843+grey1001@users.noreply.github.com>
2023-07-11 15:04:58 +0200

    Update start_httpd.sh

--- load-balancing/elb-v2/start_httpd.sh
+++ load-balancing/elb-v2/start_httpd.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Here is where you'd want to start your http daemon. For example:
-#service httpd start
+sudo systemctl start apache2
 #exit $?
 
 # In this case, since it's just a placeholder, we don't need to do anything.
