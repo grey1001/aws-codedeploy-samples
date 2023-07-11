81b30100e0fc86903837f9ec7eb7bc75a36d48a3
grey1001 <130705843+grey1001@users.noreply.github.com>
2023-07-11 15:05:42 +0200

    Update stop_httpd.sh

--- load-balancing/elb-v2/stop_httpd.sh
+++ load-balancing/elb-v2/stop_httpd.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Here is where you'd want to stop your http daemon. For example:
-#service httpd stop
+sudo systemctl stop apache2
 #exit $?
 
 # In this case, since it's just a placeholder, we don't need to do anything.
